eebbcf0d8ea7345bd42f5221685eef13bf36f5df
Amir.h Yeganemehr <yeganemehr@yahoo.com>
2017-05-07 16:51:58 +0430

    remove name for import formAjax

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ import Router from "./Router";
 import Translator from "./Translator";
 import { AjaxRequest } from "./AjaxRequest";
 import { webuilder } from "./webuilder";
+import "./formAjax";
 export {Options, AjaxRequest, Router, Translator, webuilder};
-
 
